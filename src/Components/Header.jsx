947b277ed735255815768f7da2ec1ed1d094148c
moizxox <mianmoiz899@gmail.com>
2025-01-01 13:43:02 +0500

    Header Sticky

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <>
-      <header className="bg-secondary p-4">
+      <header className="bg-secondary p-4 sticky top-0">
         <h1 className="text-2xl text-text uppercase font-bold text-center">
           Todo App
         </h1>
